769a87c4b0f1f611d8ee2a327477ca4df98567c6
Deepk kumar <deepak.kumar@hp.com>
2021-02-09 19:06:12 +0530

    progress bar using js

---
+++ JS-ShortQuestions/Progressbar/progressbar.js
@@ -0,0 +1,10 @@
+let progressBar = document.querySelector(".bar");
+
+const startProgressJS = (sec) => {
+  let percentCompleted = 0;
+  const progressInterval = setInterval(() => {
+    progressBar.style.width = percentCompleted + "%";
+    percentCompleted++;
+    if (percentCompleted > 100) clearInterval(progressInterval);
+  }, (sec * 1000) / 100);
+};
